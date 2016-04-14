935f9c61c69d4b9d5d94e70588e796232a8f402e
novovkrest <novokrest013@gmail.com>
2016-04-14 08:39:19 +0300

    modfiyf copy-out.sh after updating to UsbHostShield 2.0

--- linux/copy-out.sh
+++ linux/copy-out.sh
@@ -5,18 +5,21 @@ GitArduinoLibraries=$GitArduinoRoot/libraries
 LinArduinoRoot=~/Arduino
 LinArduinoLibraries=$LinArduinoRoot/libraries
 
+
 function acopy {
   echo $1
+  rm -rf $LinArduinoRoot/$1
   cp -r $GitArduinoRoot/$1 $LinArduinoRoot/$1
 }
 
 function alibcopy {
   echo $1
+  rm -rf $LinArduinoLibraries/$1
   cp -r $GitArduinoLibraries/$1 $LinArduinoLibraries/$1
 }
 
 acopy 'DesCryptorUsager'
-alibcopy 'DesCryptor'
+alibcopy 'DesLibrary'
 alibcopy 'UsbHostShield'
 alibcopy 'StandardCplusplus'
 
